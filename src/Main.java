4ae148623d267f995eefaa3e74da79106cc97ee2
MusienkoYuriy97 <97musienko@gmail.com>
2021-06-29 12:46:37 +0300

    commit feauture-xyz

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello22");
+        System.out.println("Hello2");
     }
 }
